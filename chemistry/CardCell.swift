e99cb2ce460ec5cc4516b9fee55696be42450992
Louis Debaere <debaere.louis@gmail.com>
2018-02-02 11:32:40 +0100

    Added Custom Cell with Image View

---
+++ chemistry/CardCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCell.swift
+//  chemistry
+//
+//  Created by Louis Debaere on 02/02/2018.
+//  Copyright © 2018 The Horcruxes. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    
+    // TODO: Gesture and Animation
+    
+}
