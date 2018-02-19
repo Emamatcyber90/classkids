322cb68cdf908094e82a54182d04f04be1060225
Louis Debaere <debaere.louis@gmail.com>
2018-02-19 16:41:29 +0100

    Removed Model

--- chemistry/2nd Game/Controller2ndGame.swift
+++ chemistry/2nd Game/Controller2ndGame.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class Controller2ndGame: UIViewController {
+    
+    
+    static let elementsArray = ["H", "O", "Na", "C", "Ca", "S", "Cl", "Cu", "F", "I", "Zn", "Au", "Sn", "P", "Br", "Ti", "Mg", "Ag", "As", "N"]
+    
+    static let formulaDictionary = [ "Camphor": "C10H16O",  "Bromine": "Br2",  "Nitrate": "NO3−",  "Carbonic Acid": "H2CO3",  "Sodium Nitrate": "NaNO3",  "Aluminium foil": "Al2O3",  "Arsenic Trioxide": "As4O3",  "Magnesium Bromide": "MgBr2",  "Copper Sulfate": "CuSO4",  "Glucose": "C6H12O6",  "Sucrose": "C12H22O11",  "Carbon Monoxide": "CO",  "Baking Soda": "NaHCO3",  "Propane": "C3H8",  "Silver Oxide": "AgI",  "Sodium Hypochlorite": "NaClO",  "Oxygen": "O2",  "Carbon Dioxide": "CO2",  "Ammonium Sulfate": "(NH4)2SO4",  "Aspirin": "C9H8O4",  "Lactic Acid": "C3H6O3",  "Nitrogen": "N2O",  "Gold Trioxide": "Au2O3",  "Butane": "C4H10",  "Pentane": "C5H12",  "Benzene": "C6H6",  "Sodium Cyanide": "NaCN",  "Hydrogen Sulfide": "H2S",  "Octane": "C8H18",  "Hydrobromic Acid": "HBr",  "Ascorbic Acid": "C6H8O6",  "Ammonia": "NH3",  "Methane": "CH4",  "Zinc": "Zn(NO3)2",  "Calcium Oxide": "CaO",  "Fluoride": "F2",  "Caffeine": "C8H10N4O2",  "Sugar": "CnH2nOn",  "Sulfate": "O4S2-",  "Vinegar": "C2H4O2",  "Hydrochloric Acid": "HCl",  "Peroxide": "H2O2",  "Calcium Cyanide": "Ca(CN)2",  "Sulfuric Acid": "H2SO4",  "Acetone": "CH3COCH3",  "Sodium Hydroxide": "NaOH",  "Cholesterol": "C27H46O",  "Ethane": "C2H6",  "Water": "H2O",  "Alcohol": "C2H6O",  "Salt": "NaCl",  "Benzoic Acid": "C7H6O2",  "Sulfurous Acid": "H2SO3",  "Methanol": "CH3OH",  "Sulfate Group": "CaSO4",  "Phosphoric Acid": "H3PO4",  "Iodic Acid": "HIO3",  "Citric Acid": "C6H8O7"]
 
+    
+    var currentElementName: String! {
+        didSet {
+            targetLabel.text = currentElementName
+            outputLabel.text?.removeAll()
+            
+            // fading animation when labels update
+            let transition = CATransition()
+            transition.type = kCATransitionFade
+            transition.duration = 0.3
+            transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            view.layer.add(transition, forKey: nil)
+        }
+    }
+    
     var tapCounter = 0
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var outputLabel: UILabel!
@@ -51,17 +71,16 @@ class Controller2ndGame: UIViewController {
         let buttonTitle = currentButton.title(for: .normal)!
         print("Element / Number: \(buttonTitle)")
         outputLabel.text?.append(buttonTitle)
-//        matchFormula()
+        //        matchFormula()
     }
-//    func updateHint(currentButton: UIButton) {
-//        let buttonTitle = currentButton.title(for: .normal)!
-//        hint.text?.append(buttonTitle)
-//    }
+    //    func updateHint(currentButton: UIButton) {
+    //        let buttonTitle = currentButton.title(for: .normal)!
+    //        hint.text?.append(buttonTitle)
+    //    }
     
     // do the matching and build the alert (optional, we can find another way to show the user he succeded
     func matchFormula() {
-        let valuesArray = Array(formulaDictionary.values)
-        let keysArray = Array(formulaDictionary.keys)
+
         let successAlert = UIAlertController(title: "Amazeballs, si fott", message: nil, preferredStyle: .alert)
         let failAlert = UIAlertController(title: "Try again!", message: nil, preferredStyle: .alert)
         let successAction = UIAlertAction(title: "OK", style: .default, handler: {action in self.nextLevel()})
@@ -69,29 +88,21 @@ class Controller2ndGame: UIViewController {
         successAlert.addAction(successAction)
         failAlert.addAction(failAction)
         
-        for i in valuesArray.indices {
-            if outputLabel.text == valuesArray[i] {
-                if targetLabel.text == keysArray[i] {
-                    present(successAlert, animated: true, completion: nil)
-                print("Match")
-                }
-            } else {
-//                present(failAlert, animated: true, completion: nil)       I get an error here, don't know y
-            }
+        if outputLabel.text! == Controller2ndGame.formulaDictionary[currentElementName!]! {
+            present(successAlert, animated: true, completion: nil)
+            print("Match")
+        } else {
+            present(failAlert, animated: true, completion: nil)
+            print("No Match")
         }
+
     }
     
     // display a new target and clear the user's inputs
     func nextLevel() {
-        targetLabel.text = randomValue()
-        outputLabel.text?.removeAll()
-        
-        // fading animation when labels update
-        let transition = CATransition()
-        transition.type = kCATransitionFade
-        transition.duration = 0.3
-        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
-        view.layer.add(transition, forKey: nil)
+        let elementCount = UInt32(Controller2ndGame.formulaDictionary.count)
+        let randomIndex = Int(arc4random_uniform(elementCount))
+        currentElementName = Array(Controller2ndGame.formulaDictionary.keys)[randomIndex]
     }
     
     override func viewDidLoad() {
@@ -102,5 +113,5 @@ class Controller2ndGame: UIViewController {
         hint.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         super.viewDidLoad()
     }
-
+    
 }
