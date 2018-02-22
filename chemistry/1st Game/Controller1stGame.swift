366c032c7ec0570107263fb8e88236ae86aab1ee
Alex Falgiano <34335719+EvansXIV@users.noreply.github.com>
2018-02-22 10:00:25 +0100

    motherfuckin table works

--- chemistry/1st Game/Controller1stGame.swift
+++ chemistry/1st Game/Controller1stGame.swift
@@ -55,8 +55,8 @@ class Controller1stGame: UIViewController {
         collectionView.backgroundColor = .clear
         Controller1stGame.elementNameForNumber = setupElementNameForNumber()
         
-        setupOriginalTable()
-        setupAdditionalTable()
+//        setupOriginalTable()
+//        setupAdditionalTable()
         
         
         
@@ -97,8 +97,9 @@ class Controller1stGame: UIViewController {
                             print("Invalid tag")
                             return
                         }
+                        
                         imageView.image = UIImage(named: name)
-//                                                imageView.isHidden = true
+                                                imageView.isHidden = true
                     } else {
                         for view in view.subviews {
                             if let imageView = view as? UIImageView {
@@ -211,12 +212,17 @@ class Controller1stGame: UIViewController {
                     if currentCard.element == lastFlippedCard.element {
                         print("Elements MATCH")
                         // Highlight position in the table
-                        let elementImageView: UIView
+                        let elementImageView: UIImageView
                         if let imageView = self.tableStackView.viewWithTag(currentCard.element) {
-                            elementImageView = imageView
+                            elementImageView = imageView as! UIImageView
+                            elementImageView.image = UIImage(named: Controller1stGame.elementNameForNumber[elementImageView.tag]!)
+                            
                         } else {
-                            elementImageView = self.lathanidesActinidesStackView.viewWithTag(currentCard.element)!
+                            elementImageView = self.lathanidesActinidesStackView.viewWithTag(currentCard.element)! as! UIImageView
+                            elementImageView.image = UIImage(named: Controller1stGame.elementNameForNumber[elementImageView.tag]!)
+
                         }
+                        
                         elementImageView.layer.isHidden = false
                         UIView.animate(withDuration: 2, animations: {
                             elementImageView.layer.zPosition = CGFloat(self.level.count / 2 - self.elementsToGo) + 1
