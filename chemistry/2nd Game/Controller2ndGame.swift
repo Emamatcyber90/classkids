4a27391904163e67dd1dfe51e72a59a417f6029e
Alex Falgiano <34335719+EvansXIV@users.noreply.github.com>
2018-02-27 18:13:13 +0100

    • Divided the level by category. For now it's just based on formula length, I'll study chemistry tomorrow and improve it. When you complete a level the targetLabel text becomes blue to indicate we're on level 2
    • Cleaned up the code for 2ndGameController

--- chemistry/2nd Game/Controller2ndGame.swift
+++ chemistry/2nd Game/Controller2ndGame.swift
@@ -12,6 +12,8 @@ import AVFoundation
 class Controller2ndGame: UIViewController {
     var player: AVAudioPlayer?
     
+    
+    var currentLevel: Int = 1
     var hintTapped = false
     var matchesCounter: Int = 0
     var failsCounter: Int = 0
@@ -23,13 +25,23 @@ class Controller2ndGame: UIViewController {
     @IBOutlet weak var periodicTable: UIImageView!
     var touched = false
     
+    var currentDictionary: [String : String] {
+        switch currentLevel {
+        case 1:
+            return Controller2ndGame.formulaDictionary1Level
+        default:
+            return Controller2ndGame.formulaDictionary2Level
+        }
+    }
+    
     let arr = ["exp1.png","exp2.png","exp3.png","exp4.png","exp5.png","exp6.png","exp7.png","exp8.png","exp9.png","exp10.png","exp11.png","exp12.png","exp13.png","exp14.png","exp15.png","exp16.png","exp17.png","exp18.png","exp19.png","exp20.png","exp21.png","exp22.png","exp23.png","exp24.png","exp25.png"]
     let arr2 = ["Bubbles0001.png","Bubbles0002.png","Bubbles0003.png","Bubbles0004.png","Bubbles0005.png","Bubbles0006.png","Bubbles0007.png","Bubbles0008.png","Bubbles0009.png","Bubbles0010.png","Bubbles0011.png","Bubbles0012.png","Bubbles0013.png","Bubbles0014.png","Bubbles0015.png","Bubbles0016.png","Bubbles0017.png","Bubbles0018.png","Bubbles0019.png","Bubbles0020.png","Bubbles0021.png","Bubbles0022.png","Bubbles0023.png","Bubbles0024.png","Bubbles0025.png","Bubbles0026.png","Bubbles0027.png","Bubbles0028.png","Bubbles0029.png","Bubbles0030.png","Bubbles0031.png","Bubbles0032.png","Bubbles0033.png","Bubbles0034.png","Bubbles0035.png","Bubbles0036.png","Bubbles0037.png","Bubbles0038.png","Bubbles0039.png","Bubbles0040.png","Bubbles0041.png","Bubbles0042.png","Bubbles0043.png","Bubbles0044.png","Bubbles0045.png","Bubbles0046.png","Bubbles0047.png","Bubbles0048.png","Bubbles0049.png","Bubbles0050.png","Bubbles0051.png"]
     
     static let elementsArray = ["H", "O", "Na", "C", "Ca", "S", "Cl", "Cu", "F", "I", "Zn", "Au", "Sn", "P", "Br", "Ti", "Mg", "Ag", "As", "N"]
     
-    static let formulaDictionary = [ "Camphor": "C10H16O",  "Bromine": "Br2",  "Carbonic Acid": "H2CO3",  "Sodium Nitrate": "NaNO3",  "Arsenic Trioxide": "As4O3",  "Magnesium Bromide": "MgBr2",  "Copper Sulfate": "CuSO4",  "Glucose": "C6H12O6",  "Sucrose": "C12H22O11",  "Carbon Monoxide": "CO",  "Baking Soda": "NaHCO3",  "Propane": "C3H8",  "Silver Oxide": "AgI",  "Sodium Hypochlorite": "NaClO",  "Oxygen": "O2",  "Carbon Dioxide": "CO2",  "Aspirin": "C9H8O4",  "Lactic Acid": "C3H6O3",  "Nitrous Oxide": "N2O",  "Gold Trioxide": "Au2O3",  "Butane": "C4H10",  "Pentane": "C5H12",  "Benzene": "C6H6",  "Sodium Cyanide": "NaCN",  "Hydrogen Sulfide": "H2S",  "Octane": "C8H18",  "Hydrobromic Acid": "HBr",  "Ascorbic Acid": "C6H8O6",  "Ammonia": "NH3",  "Methane": "CH4",  "Calcium Oxide": "CaO", "Calcium Carbonate" : "CaCO3", "Fluoride": "F2",  "Caffeine": "C8H10N4O2",  "Sugar": "CnH2nOn", "Vinegar": "C2H4O2",  "Hydrochloric Acid": "HCl",  "Hydrogen Peroxide": "H2O2", "Sulfuric Acid": "H2SO4",  "Acetone": "CH3COCH3",  "Sodium Hydroxide": "NaOH",  "Cholesterol": "C27H46O",  "Ethane": "C2H6",  "Water": "H2O",  "Alcohol": "C2H6O",  "Salt": "NaCl",  "Benzoic Acid": "C7H6O2",  "Sulfurous Acid": "H2SO3",  "Methanol": "CH3OH",  "Sulfate Group": "CaSO4",  "Phosphoric Acid": "H3PO4",  "Iodic Acid": "HIO3",  "Citric Acid": "C6H8O7", "Magnesium Sulfate" : "MgSO47H2O", "Ammonium Chloride" : "NH4Cl", "Chloroform" : "CHCl3", "Trinitrotoluene" : "C7H5N3O6"]
+    static let formulaDictionary1Level = ["Bromine": "Br2", "Carbonic Acid": "H2CO3", "Sodium Nitrate": "NaNO3", "Arsenic Trioxide": "As4O3", "Magnesium Bromide": "MgBr2",  "Copper Sulfate": "CuSO4", "Carbon Monoxide": "CO", "Baking Soda": "NaHCO3", "Propane": "C3H8", "Silver Oxide": "AgI", "Sodium Hypochlorite": "NaClO", "Oxygen": "O2", "Carbon Dioxide": "CO2", "Nitrous Oxide": "N2O", "Gold Trioxide": "Au2O3", "Butane": "C4H10", "Pentane": "C5H12", "Benzene": "C6H6", "Sodium Cyanide": "NaCN", "Hydrogen Sulfide": "H2S", "Octane": "C8H18", "Hydrobromic Acid": "HBr", "Ammonia": "NH3", "Methane": "CH4", "Calcium Oxide": "CaO", "Calcium Carbonate" : "CaCO3", "Fluoride": "F2", "Hydrochloric Acid": "HCl", "Hydrogen Peroxide": "H2O2", "Sulfuric Acid": "H2SO4", "Sodium Hydroxide": "NaOH", "Ethane": "C2H6", "Water": "H2O", "Alcohol": "C2H6O", "Salt": "NaCl", "Benzoic Acid": "C7H6O2", "Sulfurous Acid": "H2SO3", "Methanol": "CH3OH", "Sulfate Group": "CaSO4", "Phosphoric Acid": "H3PO4", "Iodic Acid": "HIO3", "Ammonium Chloride" : "NH4Cl"]
 
+    static let formulaDictionary2Level = ["Camphor": "C10H16O", "Glucose": "C6H12O6", "Sucrose": "C12H22O11", "Aspirin": "C9H8O4", "Lactic Acid": "C3H6O3", "Caffeine": "C8H10N4O2", "Sugar": "CnH2nOn", "Vinegar": "C2H4O2", "Acetone": "CH3COCH3", "Cholesterol": "C27H46O", "Citric Acid": "C6H8O7", "Magnesium Sulfate" : "MgSO47H2O", "Chloroform" :"CHCl3", "Ascorbic Acid": "C6H8O6", "Trinitrotoluene" : "C7H5N3O6"]
     
     var currentElementName: String! {
         didSet {
@@ -103,6 +115,7 @@ class Controller2ndGame: UIViewController {
         view.layer.add(transition, forKey: nil)
     }
     
+    
     @IBAction func skipLevel(_ sender: UIButton) {
         nextLevel()
     }
@@ -115,6 +128,7 @@ class Controller2ndGame: UIViewController {
             })
     }
     
+    
     @IBAction func changeTable(_ sender: UITapGestureRecognizer) {
         if touched == false {
             periodicTable.image = UIImage(named: "Tab1")
@@ -125,6 +139,7 @@ class Controller2ndGame: UIViewController {
         }
     }
     
+    
     @IBAction func hideTable(_ sender: UITapGestureRecognizer) {
         UIView.animate(withDuration: 0.5, animations: {
             self.periodicTable.transform = CGAffineTransform(scaleX: 1, y: 1)
@@ -133,35 +148,30 @@ class Controller2ndGame: UIViewController {
         })
     }
     
+    
     // add the element's text in the outputLabel
     func updateLabel(currentButton: UIButton) {
         let buttonTitle = currentButton.title(for: .normal)!
         print("Element / Number: \(buttonTitle)")
         outputLabel.text?.append(buttonTitle)
-        //        matchFormula()
     }
-    //    func updateHint(currentButton: UIButton) {
-    //        let buttonTitle = currentButton.title(for: .normal)!
-    //        hint.text?.append(buttonTitle)
-    //    }
+    
     
     // do the matching and build the alert (optional, we can find another way to show the user he succeded
     func matchFormula() {
-
-//        let successAlert = UIAlertController(title: "Amazeballs, si fott", message: nil, preferredStyle: .alert)
-//        let failAlert = UIAlertController(title: "Try again!", message: nil, preferredStyle: .alert)
-//        let successAction = UIAlertAction(title: "OK", style: .default, handler: {action in self.nextLevel()})
-//        let failAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-//        successAlert.addAction(successAction)
-//        failAlert.addAction(failAction)
         
-        if outputLabel.text! == Controller2ndGame.formulaDictionary[currentElementName!]! {
+        if outputLabel.text! == currentDictionary[currentElementName!]! {
             bubbleSound()
             matchSucces.animationDuration = 1
             matchSucces.animationRepeatCount = 1
             matchSucces.startAnimating()
             matchesCounter += 1
-            print(matchesCounter)
+            print("Matches: \(matchesCounter)")
+            if matchesCounter > 2 {
+                currentLevel = 2
+                targetLabel.textColor = UIColor.blue
+                
+            }
             nextLevel()
         } else {
             explosionSound()
@@ -183,21 +193,20 @@ class Controller2ndGame: UIViewController {
     // display a new target and clear the user's inputs
     func nextLevel() {
         failsCounter = 0
-        matchesCounter = 0
         hintLabel.isHidden = true
         hintButton.isHidden = true
         
         hintButton.isUserInteractionEnabled = true
         hintButton.alpha = 1
         
-        let elementCount = UInt32(Controller2ndGame.formulaDictionary.count)
+        let elementCount = UInt32(currentDictionary.count)
         let randomIndex = Int(arc4random_uniform(elementCount))
-        currentElementName = Array(Controller2ndGame.formulaDictionary.keys)[randomIndex]
-        solution = Array(Controller2ndGame.formulaDictionary.values)[randomIndex]
+        currentElementName = Array(currentDictionary.keys)[randomIndex]
+        solution = Array(currentDictionary.values)[randomIndex]
         print("Solution: \(solution)")
     }
     
-    
+    // ADD SOUNDS
     func bubbleSound() {
         let sound = Bundle.main.url(forResource: "Bubbles", withExtension: "mp3")
         do {
@@ -235,11 +244,7 @@ class Controller2ndGame: UIViewController {
         if hintTapped {
             hintButton.isUserInteractionEnabled = false
             hintButton.alpha = 0.5
-        } else {
-            hintButton.isUserInteractionEnabled = true
-            hintButton.alpha = 1
         }
-        
     }
     
     
@@ -271,8 +276,8 @@ class Controller2ndGame: UIViewController {
         super.viewDidLoad()
         periodicTable.isHidden = true
     }
-    
 }
+
 
 extension Array {
     mutating func shuffleSolution() {
